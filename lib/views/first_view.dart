a2c96de2490e427091ff6cad0a349399ba4bfdfc
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-11 10:33:53 +0200

    add describe text

--- lib/views/first_view.dart
+++ lib/views/first_view.dart
@@ -8,18 +8,38 @@ class FirstView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.only(
-          right: 32,
-          left: 32,
-          top: 64,
+        child: Padding(
+          padding: EdgeInsets.only(
+            right: 32,
+            left: 32,
+            top: 32,
+          ),
+          child: Column(
+            children: [
+              PlayersCard(),
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Dicover all about sport',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "Stay ahead of the game—live scores, instant updates, and all the action, right at your fingertips!",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            PlayersCard(),
-          ],
-        ),
-      )),
+      ),
     );
   }
 }
