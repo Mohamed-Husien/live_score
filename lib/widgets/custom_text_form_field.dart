6663105daabbeaa4f6cfb4d5ad46e99d9fdaca4b
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-12 09:37:39 +0200

    add password obscure icon

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -2,19 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:live_score_app/constant.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField(
-      {super.key,
-      required this.hint,
-      this.maxLines = 1,
-      this.onSaved,
-      this.onChanged});
+  const CustomTextFormField({
+    super.key,
+    required this.hint,
+    this.maxLines = 1,
+    this.onSaved,
+    this.onChanged,
+    this.obscureText = false,
+    this.suffixIcon,
+  });
   final String hint;
   final int maxLines;
   final void Function(String?)? onSaved;
   final Function(String)? onChanged;
+  final bool obscureText;
+  final Widget? suffixIcon;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: obscureText,
       onChanged: onChanged,
       onSaved: onSaved,
       validator: (value) {
@@ -26,11 +32,15 @@ class CustomTextFormField extends StatelessWidget {
       },
       maxLines: maxLines,
       cursorColor: kPrimaryColor,
+      style: const TextStyle(
+        color: Colors.white, // <-- Change text color to white
+      ),
       decoration: InputDecoration(
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
         hintText: hint,
+        suffixIcon: suffixIcon,
         hintStyle: const TextStyle(
           color: kPrimaryColor,
         ),
--- lib/widgets/signin_show_model_bottom_sheet_body.dart
+++ lib/widgets/signin_show_model_bottom_sheet_body.dart
@@ -2,28 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:live_score_app/widgets/custom_semi_bold_text.dart';
 import 'package:live_score_app/widgets/custom_text_form_field.dart';
 
-class SignInShowModelBottomSheetBody extends StatelessWidget {
+class SignInShowModelBottomSheetBody extends StatefulWidget {
   const SignInShowModelBottomSheetBody({super.key});
 
+  @override
+  State<SignInShowModelBottomSheetBody> createState() =>
+      _SignInShowModelBottomSheetBodyState();
+}
+
+class _SignInShowModelBottomSheetBodyState
+    extends State<SignInShowModelBottomSheetBody> {
+  bool isSecurePassword = true;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
       child: ListView(
-        children: const [
-          CustomSemiBoldText(text: "Welcome", fontSize: 28),
-          SizedBox(
+        children: [
+          const CustomSemiBoldText(text: "Welcome", fontSize: 28),
+          const SizedBox(
+            height: 24,
+          ),
+          const CustomTextFormField(
+            hint: 'Email',
+          ),
+          const SizedBox(
             height: 24,
           ),
           CustomTextFormField(
-            hint: 'Email',
+            obscureText: isSecurePassword,
+            suffixIcon: toggalePassword(),
+            hint: 'Password',
           ),
-          SizedBox(
-            height: 24,
-          ),
-          CustomTextFormField(hint: 'Password'),
         ],
       ),
     );
+  }
+
+  Widget toggalePassword() {
+    return IconButton(
+        onPressed: () {
+          setState(() {
+            isSecurePassword = !isSecurePassword;
+          });
+        },
+        icon: isSecurePassword
+            ? const Icon(Icons.visibility)
+            : const Icon(
+                Icons.visibility_off,
+                color: Colors.grey,
+              ));
   }
 }
